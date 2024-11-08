b89d913c5a4881e4cb6171c5ec54699131e8f9b5
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-08 00:28:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-# print("test")
-# print("test")
-# print("test")
-# print("test")
-# print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
+print("test")
 
-# x = 2
+x = 2
 
-# x = 0
-# for i in range(3):
-#     pass
-# print("hello world!")
+x = 0
+for i in range(3):
+    pass
+print("hello world!")
 
 
 
