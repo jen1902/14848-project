426ea106ce50cc72b14a21599f17ca84a46aaa30
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 00:47:25 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,28 +1,16 @@
 import sys
 from collections import defaultdict
 
-def parse(line):
-    word, fp, ct = line.strip().split("\t")
-    return word, fp, int(ct)
+# Initialize a dictionary to store term frequencies
+term_dict = defaultdict(lambda: defaultdict(int))
 
-def updateWordDict(wordDict, word, fp, ct):
-    wordDict[word][fp] += ct
+# Reducer function
+for line in sys.stdin:
+    term, filepath, count = line.strip().split("\t")
+    count = int(count)
+    term_dict[term][filepath] += count
 
-def format(postings):
-    return ' '.join([f"{doc}:{freq}" for doc, freq in postings.items()])
-
-def outputInvertIdx(wordDict):
-    for word, postings in wordDict.items():
-        postings_list = format(postings)
-        print(f"{word}\t{{{postings_list}}}")
-
-def reducer():
-    wordDict = defaultdict(lambda: defaultdict(int))
-    for line in sys.stdin:
-        word, fp, ct = parse(line)
-        updateWordDict(wordDict, word, fp, ct)
-    outputInvertIdx(wordDict)
-
-# Run reducer
-if __name__ == "__main__":
-    reducer()
+# Output the final inverted index
+for term, postings in term_dict.items():
+    postings_list = ', '.join([f"{doc}:{freq}" for doc, freq in postings.items()])
+    print(f"{term}\t{{{postings_list}}}")
