ed0b69d65b6f094120737f9987c86517c0ea076e
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 01:26:14 -0500

    Update run_hadoop_setup.sh

--- run_hadoop_setup.sh
+++ run_hadoop_setup.sh
@@ -1,10 +1,9 @@
-git clone https://github.com/jen1902/14848-project.git
-&& echo "enter project"
-
-&& cd 14848-project
-&& echo "remove"
-&& hadoop fs -rm /input_data.txt
-&& hadoop fs -put /input_data.txt /
-&& hadoop jar /usr/lib/hadoop/hadoop-streaming.jar     -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py'     -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py'     -input /input_data.txt -output /output10
-&& hadoop fs -getmerge /output10 result4
-&& cat result4
+git clone https://github.com/jen1902/14848-project.git && \
+                    echo 'enter project' && \
+                    cd 14848-project && \
+                    echo 'remove' && \
+                    hadoop fs -rm /input_data.txt && \
+                    hadoop fs -put /input_data.txt / && \
+                    hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py' -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py' -input /input_data.txt -output /output11 && \
+                    hadoop fs -getmerge /output11 result5 && \
+                    cat result5
