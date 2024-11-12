b24bb9c9b2e4e59022bd6c9ee66a3d20613836a7
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 00:42:22 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,16 +1,28 @@
 import sys
-current_key = None
-current_count = 0
-for line in sys.stdin:
-    key, count = line.split()
-    count = int(count)
-    if key == current_key:
-        current_count += count
-    else:
-        if current_key:
-            print(f"{current_key} {current_count}")
-        current_key = key
-        current_count = count
-# Output the last key-value pair
-if current_key:
-    print(f"{current_key} {current_count}")
+from collections import defaultdict
+
+def parse(line):
+    word, fp, ct = line.strip().split("\t")
+    return word, fp, int(ct)
+
+def updateWordDict(wordDict, word, fp, ct):
+    wordDict[word][fp] += ct
+
+def format(postings):
+    return ' '.join([f"{doc}:{freq}" for doc, freq in postings.items()])
+
+def outputInvertIdx(wordDict):
+    for word, postings in wordDict.items():
+        postings_list = format(postings)
+        print(f"{word}\t{{{postings_list}}}")
+
+def reducer():
+    wordDict = defaultdict(lambda: defaultdict(int))
+    for line in sys.stdin:
+        word, fp, ct = parse(line)
+        updateWordDict(wordDict, word, fp, ct)
+    outputInvertIdx(wordDict)
+
+# Run reducer
+if __name__ == "__main__":
+    reducer()
