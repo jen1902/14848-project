8416d75d8111df72c54f9f422355bb928db9907e
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 01:23:18 -0500

    Update run_hadoop_setup.sh

--- run_hadoop_setup.sh
+++ run_hadoop_setup.sh
@@ -1,6 +1,10 @@
 git clone https://github.com/jen1902/14848-project.git
-cd 14848-project
-hadoop fs -put /input_data.txt /
-hadoop jar /usr/lib/hadoop/hadoop-streaming.jar     -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py'     -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py'     -input /input_data.txt -output /output9
-hadoop fs -getmerge /output9 result3
-cat result3
+&& echo "enter project"
+
+&& cd 14848-project
+&& echo "remove"
+&& hadoop fs -rm /input_data.txt
+&& hadoop fs -put /input_data.txt /
+&& hadoop jar /usr/lib/hadoop/hadoop-streaming.jar     -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py'     -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py'     -input /input_data.txt -output /output10
+&& hadoop fs -getmerge /output10 result4
+&& cat result4
