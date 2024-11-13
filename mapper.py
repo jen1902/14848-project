f548547c62da5497484fdb476310e245160783b1
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 22:03:51 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -6,24 +6,24 @@ def get_file():
 
 def ct_terms(line):
     words = line.strip().split()
-    wordCounts = {}
+    word_ct = {}
     for w in words:
         word = w.lower()
-        if word in wordCounts:
-            wordCounts[word] += 1
+        if word in word_ct:
+            word_ct[word] += 1
         else:
-            wordCounts[word] = 1
-    return wordCounts
+            word_ct[word] = 1
+    return word_ct
 
-def word_ct_output(filepath, wordCounts):
-    for word, ct in wordCounts.items():
+def word_ct_output(filepath, word_ct):
+    for word, ct in word_ct.items():
         print(f"{word}\t{filepath}\t{ct}")
 
 def mapper():
     fp = get_file()
     for line in sys.stdin:
-        wordCounts = ct_terms(line)
-        word_ct_output(fp, wordCounts)
+        word_ct = ct_terms(line)
+        word_ct_output(fp, word_ct)
 
 # Run mapper
 if __name__ == "__main__":
