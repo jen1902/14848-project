8ae1f7611b6e798915e8dc048199dc69595a2ef3
Jennifer Wang <68262181+jen1902@users.noreply.github.com>
2024-11-12 22:03:29 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -5,23 +5,23 @@ def parse(line):
     word, fp, ct = line.strip().split("\t")
     return word, fp, int(ct)
 
-def update_word_dict(wordDict, word, fp, ct):
-    wordDict[word][fp] += ct
+def update_word_dict(word_dict, word, fp, ct):
+    word_dict[word][fp] += ct
 
 def format(postings):
     return ' '.join([f"{doc}:{freq}" for doc, freq in postings.items()])
 
-def invert_idx_output(wordDict):
-    for word, postings in wordDict.items():
+def invert_idx_output(word_dict):
+    for word, postings in word_dict.items():
         postings_list = format(postings)
         print(f"{word}\t{{{postings_list}}}")
 
 def reducer():
-    wordDict = defaultdict(lambda: defaultdict(int))
+    word_dict = defaultdict(lambda: defaultdict(int))
     for line in sys.stdin:
         word, fp, ct = parse(line)
-        update_word_dict(wordDict, word, fp, ct)
-    invert_idx_output(wordDict)
+        update_word_dict(word_dict, word, fp, ct)
+    invert_idx_output(word_dict)
 
 # Run reducer
 if __name__ == "__main__":
